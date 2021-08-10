3cff6e343eaf8291f5668d1e7558adcfe242c074
Brian-mula <mulatiwangilabrian@gmail.com>
2021-08-10 20:18:42 +0300

    init

---
+++ database/migrations/2021_08_08_051736_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('sub_category_id')->constrained();
+            $table->integer('price');
+            $table->integer('discount');
+            $table->integer('actual_price');
+            $table->integer('quantity');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
